61f4a3de77296756a1181af7c98d75481bbded46
33322654 <160145630+33322654@users.noreply.github.com>
2024-05-11 11:42:12 -0500

    Update principal.cpp

--- principal.cpp
+++ principal.cpp
@@ -1,88 +1,53 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int
-main ()
-{
-  int op, recurso, id, prestamo, informacion_del_prestamo, recurso_prestado, recurso_numero;
-  string estudiante;
-  
-  cout << "Bienvenido a la central de prestamos de la Universidad" << endl;
-  do
-	{
-	  cout << "Ingresa:\n" <<
-	    "1)agregar un recurso\n" <<
-		"2)agregar un estudiante\n" << 
-		"3)prestar un recurso dispinible\n" <<
-		"4)consultar los prestamos de un estudiante\n" <<
-		"5)consultar la informacion de un prestamo\n" <<
-		"6)devolver un recurso prestado\n" << "7) salir\n" << endl;
-	  cin >> op;
 
-	  switch (op)
-		{
-		    case 1:
-		        {
-			      cout << "Agregar un recurso" << endl;
-			      cout<< "ingrese numero del recurso"<<endl;
-			      cin >> recurso;
-			      cout << "Se agrego correctamente" << recurso << endl;
-		         }break;
+// Función para agregar un recurso al sistema
+int agregarRecurso(int recurso) {
+    cout << "Ingresar número del recurso: ";
+    cin >> recurso;
+    cout << "Se agregó correctamente el recurso número " << recurso << endl;
+    return recurso;
+}
 
-		     case 2:
-		          {
-			        cout << "Agregar un estudiante" << endl;
-		         	cout << "Ingresar id estudiante: "<< endl;
-			        cin >> id;
-		            cout << "Ingresar el codigo del estudiante" << endl;
-	                cin >> estudiante;
-	                cout<<"se agrego correctamente el recurso numero" << estudiante << "con id" << id << endl;
- 
-		          }break;
-	
+// Función para agregar un estudiante al sistema
+int agregarEstudiante(int& id, string& estudiante) {
+    cout << "Ingresar ID del estudiante: ";
+    cin >> id;
+    cout << "Ingresar código del estudiante: ";
+    cin >> estudiante;
+    cout << "Se agregó correctamente el estudiante con ID " << id << " y código " << estudiante << endl;
+    return id;
+}
 
-               case 3:
-                    {
-	                    cout << "prestar un recurso disponible" << endl;
-	                    cin >> recurso;
-                    }break;
-                case 4:
-                     {
-	                     cout << "consultar los prestamos de un estudiante" << endl;
-	                     cin >> prestamo;
-                      }break;
-                      
-                case 5:
-                    {
-	                    cout << "consultar la informacion de un prestamo" << endl;
-	                    cin >> informacion_del_prestamo;
-                    }break;
-                    
-                case 6:
-                    {
-                    	cout << "devolver un recurso prestado" << endl;
-	                    cin >> recurso_prestado;
+// Función principal del programa
+int main() {
+    int op, recurso, id;
+    string estudiante;
 
-                    }break;
-                    
-                case 7:
-                    {
-	                     cout << "Gracias por utilizar la Central de prestamos" << endl;
-                    }break;
- 
-                default:
-                    {
-	                     cout << "Ingresa una opciC3n vC!lida" << endl;
-                    }break;
-  
+    cout << "Bienvenido a la central de préstamos de la Universidad" << endl;
+
+    while (op != 7) {
+        cout << "\nIngresa una opción:\n"
+             << "1) Agregar un recurso\n"
+             << "2) Agregar un estudiante\n"
+             << "3) Prestar un recurso disponible\n"
+             << "4) Consultar los préstamos de un estudiante\n"
+             << "5) Consultar la información de un préstamo\n"
+             << "6) Devolver un recurso prestado\n"
+             << "7) Salir\n";
+        cin >> op;
+
+        if (op == 1) {
+            recurso = agregarRecurso(recurso);
+        } else if (op == 2) {
+            id = agregarEstudiante(id, estudiante);
+        } else if (op == 7) {
+            cout << "Gracias por utilizar la Central de préstamos" << endl;
+        } else {
+            cout << "Ingresa una opción válida" << endl;
         }
-	}while (op != 7);
-  
-        cout << "Ingresa:\n" << "1)agregar un recurso\n" <<
-		"2)agregar un estudiante\n" << "3)prestar un recurso dispinible\n" <<
-		"4)consultar los prestamos de un estudiante\n" <<
-		"5)consultar la informacion de un prestamo\n" <<
-		"6)devolver un recurso prestado\n" << "7) salir\n" << endl;
-  return 0;
-  
+    }
+
+    return 0;
 }
